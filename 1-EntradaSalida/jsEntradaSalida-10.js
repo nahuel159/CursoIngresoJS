64230f513fec5f7a5ca1c227e3e64685259648f1
nahuel159 <52472555+nahuel159@users.noreply.github.com>
2019-07-03 22:24:01 -0300

    Add files via upload
    clase de programacion

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,12 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+	var importe
+    var aumento;
+    var nuevosueldo;
+    
+   importe = parseFloat(document.getElementById("importe").value);
+   aumento = importe *25/100;
+   nuevosueldo = importe - aumento;
+   document.getElementById("resultado").value = nuevosueldo;
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,13 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
-
+    var sueldo;
+    var aumento;
+    var nuevosueldo;
+    
+   sueldo = parseFloat(document.getElementById("sueldo").value);
+   aumento = sueldo * .1;
+   nuevosueldo = sueldo + aumento;
+   document.getElementById("resultado").value = nuevosueldo;
 	
 }
