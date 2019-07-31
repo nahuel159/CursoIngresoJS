1868d2a667ac7089320fdc141d0769492abc3645
nahuel159 <52472555+nahuel159@users.noreply.github.com>
2019-07-31 20:16:58 -0300

    Add files via upload
    clase Switch

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -1,9 +1,26 @@
 function mostrar()
 {
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+	//tomo la edad  
+	var mes = document.getElementById('mes').value;
 
+	switch(mes){
+		case "Enero":
+			alert("Que comiences bien el año!");
+			break;
+	
+		case "Marzo":
+			alert("A clases!");
+			break;
 
+		case "Julio":
+			alert("Se vienen las vacaciones!");
+			break;
 
+		case "Diciembre":
+			alert("Felices fiestas!");
+			break;
 
+		//default:
+		//	alert("No es Enero, ni Marzo, ni Julio, ni Diciembre...");
+	}
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,12 +1,27 @@
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+var mes = document.getElementById('mes').value;
 
-//alert (mesDelAño);
-	
-	
+switch (mes)
+{
+    case "Febrero":
+        alert ("Este mes tiene 29 días");
+    break;
 
+    case "Enero":
+    case "Marzo":
+    case "Mayo":
+    case "Julio":
+    case "Agosto":
+    case "Octubre":
+    case "Diciembre":
+        alert ("Este mes tiene 31 días");
+    break;
 
+    default:
+        alert ("Este mes tiene 30 días");
+
+}
 
 }//FIN DE LA FUNCIÓN
